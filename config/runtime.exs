31317de63aede49af913403ed01843f9bf948645
Jonatan Männchen <jonatan@maennchen.ch>
2024-08-09 10:19:11 +0200

    Format Code

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,13 +1,6 @@
 import Config
 
 if config_env() == :test do
-  config :warder, Warder.Repo,
-    port: System.get_env("DATABASE_PORT", "5432"),
-    username: System.get_env("DATABASE_USER", "warder"),
-    password: System.get_env("DATABASE_PASSWORD", ""),
-    database: System.get_env("DATABASE_NAME", "warder_#{config_env()}"),
-    hostname: System.get_env("DATABASE_HOST", "localhost")
-
   max_runs =
     case System.fetch_env("CI") do
       :error -> 100
@@ -15,4 +8,11 @@ if config_env() == :test do
     end
 
   config :stream_data, max_runs: max_runs
+
+  config :warder, Warder.Repo,
+    port: System.get_env("DATABASE_PORT", "5432"),
+    username: System.get_env("DATABASE_USER", "warder"),
+    password: System.get_env("DATABASE_PASSWORD", ""),
+    database: System.get_env("DATABASE_NAME", "warder_#{config_env()}"),
+    hostname: System.get_env("DATABASE_HOST", "localhost")
 end
