1a8c10cdcb6e3b5846e496e8c6efe929449d10b6
Jonatan Männchen <jonatan@maennchen.ch>
2024-08-13 10:44:43 +0200

    Release v0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Warder.MixProject do
   def project do
     [
       app: :warder,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
